ba6be44484a5d4d63b5733fea8031bdedd84e04c
sahadathhredoy <sahadat@encodersbd.com>
2019-12-09 17:49:51 +0600

    Inital

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shortnr.net - URL shortener built in ASP.NET
+
+This is a very simple URL shortener built in the ASP.NET MVC and Web API frameworks. This code is part of a tutorial: https://ducode.org/posts/tutorial-url-shortner-asp-net/
+
+![Shortnr.net in action](/screen.png)
+
+For installation instructions, read the article mentioned above.
---
+++ Shortnr.Web.Data/ShortnrContext.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.EntityFramework;
+using Shortnr.Web.Entities;
+using System.Data.Entity;
+
+namespace Shortnr.Web.Data
+{
+
+	public class ShortnrContext : DbContext
+	{
+		public ShortnrContext()
+			: base("name=Shortnr")
+		{
+
+		}
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Stat>()
+				.HasRequired(s => s.ShortUrl)
+				.WithMany(u => u.Stats)
+				.Map(m => m.MapKey("shortUrl_id"));
+		}
+
+		public virtual DbSet<ShortUrl> ShortUrls { get; set; }
+		public virtual DbSet<Stat> Stats { get; set; }
+	}
+}
